299f862e16034977d90e99d29a955afc15d548d4
samuele.fulcheri <samuele.fulcheri@itiscuneo.eu>
2023-02-16 12:34:19 +0100

    Sixth commit

--- src/stocker/storage/view/component/SSScrollPane.java
+++ src/stocker/storage/view/component/SSScrollPane.java
@@ -1,4 +1,5 @@
 package stocker.storage.view.component;
+import org.jetbrains.annotations.NotNull;
 import stocker.storage.view.Windows;
 import javax.swing.*;
 import javax.swing.plaf.basic.BasicScrollBarUI;
@@ -8,83 +9,54 @@ public class SSScrollPane extends JScrollPane {
     public SSScrollPane(Component component) {
         super(component);
 
-        JScrollBar verticalScrollBar = getVerticalScrollBar();
-        verticalScrollBar.setUI(new BasicScrollBarUI() {
-            @Override
-            protected JButton createDecreaseButton(int orientation) {
-                return new JButton() {
-                    @Override
-                    public Dimension getPreferredSize() {
-                        return new Dimension(0, 0);
-                    }
-                };
-            }
-
-            @Override
-            protected JButton createIncreaseButton(int orientation) {
-                return new JButton() {
-                    @Override
-                    public Dimension getPreferredSize() {
-                        return new Dimension(0, 0);
-                    }
-                };
-            }
-
-            @Override
-            protected void paintTrack(Graphics g, JComponent c, Rectangle trackBounds) {
-                g.setColor(Windows.GRIGIO_CHIARO);
-                g.fillRect(trackBounds.x, trackBounds.y, trackBounds.width, trackBounds.height);
-            }
-
-            @Override
-            protected void paintThumb(Graphics g, JComponent c, Rectangle thumbBounds) {
-                g.setColor(Windows.GRIGIO_SCURO);
-                ((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                g.fillRoundRect(thumbBounds.x, thumbBounds.y, thumbBounds.width, thumbBounds.height, 10, 10);
-            }
-        });
-
-        JScrollBar horizontalScrollBar = getHorizontalScrollBar();
-        horizontalScrollBar.setUI(new BasicScrollBarUI() {
-            @Override
-            protected JButton createDecreaseButton(int orientation) {
-                return new JButton() {
-                    @Override
-                    public Dimension getPreferredSize() {
-                        return new Dimension(0, 0);
-                    }
-                };
-            }
-
-            @Override
-            protected JButton createIncreaseButton(int orientation) {
-                return new JButton() {
-                    @Override
-                    public Dimension getPreferredSize() {
-                        return new Dimension(0, 0);
-                    }
-                };
-            }
-
-            @Override
-            protected void paintTrack(Graphics g, JComponent c, Rectangle trackBounds) {
-                g.setColor(Windows.GRIGIO_CHIARO);
-                g.fillRect(trackBounds.x, trackBounds.y, trackBounds.width, trackBounds.height);
-            }
-
-            @Override
-            protected void paintThumb(Graphics g, JComponent c, Rectangle thumbBounds) {
-                g.setColor(Windows.GRIGIO_SCURO);
-                ((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                g.fillRoundRect(thumbBounds.x, thumbBounds.y, thumbBounds.width, thumbBounds.height, 10, 10);
-            }
-        });
-
         setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-        verticalScrollBar.setUnitIncrement(16);
         setBorder(null);
+
+        JScrollBar verticalScrollBar = getVerticalScrollBar();
+        verticalScrollBar.setUI(new SSScrollBarUI());
+        verticalScrollBar.setUnitIncrement(16);
         verticalScrollBar.setBackground(null);
+
+        JScrollBar horizontalScrollBar = getHorizontalScrollBar();
+        horizontalScrollBar.setUI(new SSScrollBarUI());
         horizontalScrollBar.setBackground(null);
+    }
+}
+
+class SSScrollBarUI extends BasicScrollBarUI {
+    @Override
+    protected JButton createDecreaseButton(int orientation) {
+        return new JButton() {
+            @Override
+            public Dimension getPreferredSize() {
+                return new Dimension(0, 0);
+            }
+        };
+    }
+
+    @Override
+    protected JButton createIncreaseButton(int orientation) {
+        return new JButton() {
+            @Override
+            public Dimension getPreferredSize() {
+                return new Dimension(0, 0);
+            }
+        };
+    }
+
+    // TODO: Fixare pixel bianco errore
+
+    @Override
+    protected void paintTrack(@NotNull Graphics g, JComponent c, @NotNull Rectangle trackBounds) {
+        g.setColor(Windows.GRIGIO_CHIARO);
+        g.fillRect(trackBounds.x, trackBounds.y, trackBounds.width, trackBounds.height);
+    }
+
+    @Override
+    protected void paintThumb(@NotNull Graphics g, JComponent c, @NotNull Rectangle thumbBounds) {
+        g.setColor(Windows.GRIGIO_SCURO);
+        ((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.fillRoundRect(thumbBounds.x, thumbBounds.y, thumbBounds.width, thumbBounds.height, 10, 10);
     }
 }
