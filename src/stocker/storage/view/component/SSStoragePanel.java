0225f64cfca49b501e6e87cda0e9a7b9a1b99898
samuelefulcheri <samuele.fulcheri@itiscuneo.eu>
2023-04-03 14:48:47 +0200

    Ninth commit - Samuele Fulcheri

--- src/stocker/storage/view/component/SSStoragePanel.java
+++ src/stocker/storage/view/component/SSStoragePanel.java
@@ -6,10 +6,11 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class SSStoragePanel extends JPanel implements MouseWheelListener {
     private float zoomFactor;
+    private float zoom = 1;
 
     public SSStoragePanel() {
         super();
@@ -21,7 +22,11 @@ public class SSStoragePanel extends JPanel implements MouseWheelListener {
 
         addComponentListener(new ComponentAdapter() {
             @Override
-            public void componentResized(ComponentEvent evt) {
+            public void componentResized(ComponentEvent e) {
+                var components = getComponents();
+                for(var component: components)
+                    component.revalidate();
+
                 revalidate();
             }
         });
@@ -30,32 +35,61 @@ public class SSStoragePanel extends JPanel implements MouseWheelListener {
     @Override
     public void mouseWheelMoved(@NotNull MouseWheelEvent e) {
         zoomFactor = (e.getWheelRotation() < 0) ? 1.1F : 0.9F;
+        zoom *= zoomFactor;
+
+        System.out.println(zoom);
 
         var size = getParent().getSize();
         setPreferredSize(size.width*zoomFactor, size.height*zoomFactor);
         revalidate();
+
+        var components = getComponents();
+        for(var component: components) {
+            var sizeComponent = getSize();
+            setPreferredSize(component, sizeComponent.width*zoomFactor, sizeComponent.height*zoomFactor);
+            component.revalidate();
+        }
     }
 
     private void setPreferredSize(float size1, float size2) {
         setPreferredSize(new Dimension((int) size1, (int) size2));
     }
 
+    private void setPreferredSize(@NotNull Component c, float size1, float size2) {
+        c.setPreferredSize(new Dimension((int) size1, (int) size2));
+    }
+
     public void paintStorage(boolean[][] storage) {
         removeAll();
+        var shelf = buildShelf(storage.length, storage[0].length);
+        setPreferredSize(shelf, getHeight(), getWidth());
+        add(shelf);
+        shelf.revalidate();
+        setVisible(true);
+    }
 
-        var panelWidth = getWidth()/storage.length;
-        var panelHeight = getHeight()/storage[0].length;
+    private @NotNull JPanel buildShelf(int x, int y) {
+        var screenWidth = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+        var screenHeight = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+        var objectWidth = (int) (screenWidth*0.005);
+        var objectHeight = (int) (screenHeight*0.005);
 
-        var panel = new SSPanel();
+        var shelfPanel = new JPanel(new GridLayout(y, x));
+        shelfPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 10, true));
+        shelfPanel.setBackground(Color.GRAY);
 
-        for(var i = 0; i < storage.length; i++)
-            for(var j = 0; j < storage[0].length; j++)
-                if(storage[i][j]) {
-                    panel.setBounds(i*panelWidth, j*panelHeight, panelWidth, panelHeight);
-                    add(panel);
-                }
-
-        revalidate();
-        repaint();
+        for(int i = 0; i < x*y; i++) {
+            JPanel objectPanel = new JPanel();
+            objectPanel.setPreferredSize(new Dimension(objectWidth, objectHeight));
+            objectPanel.setBackground(Color.WHITE);
+            objectPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 5, true));
+            shelfPanel.add(objectPanel);
+            if((i+1)%x == 0 && i != x*y-1) {
+                JPanel dividerPanel = new JPanel();
+                dividerPanel.setPreferredSize(new Dimension(screenWidth, 5));
+                dividerPanel.setBackground(Color.GRAY);
+                shelfPanel.add(dividerPanel);
+            }
+        } return shelfPanel;
     }
 }
