6267f10e91600be28cfea7ca51916ccd9ee2a56c
konstantinos <konstantinos.moldovanu@itiscuneo.eu>
2023-04-19 13:14:02 +0200

    Terzo commit still working

--- src/stocker/storage/view/pages/NotificationsPage.java
+++ src/stocker/storage/view/pages/NotificationsPage.java
@@ -1,21 +1,62 @@
 package stocker.storage.view.pages;
 import stocker.storage.view.SSWindow;
-import stocker.storage.view.component.SSPanel;
-import stocker.storage.view.component.SSTextArea;
+import stocker.storage.view.component.*;
+
 import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class NotificationsPage extends SSPanel {
+
+    private static final int NOTIFICATION_WIDTH = 200;
+    private static final int NOTIFICATION_HEIGHT = 300;
+
     public NotificationsPage() {
         setBorder(null);
         setBackground(SSWindow.LIGHT_GRAY);
 
+
         var gbc = new GridBagConstraints();
 
-        var notifications = new SSTextArea("Notifications");
-        gbc.weightx = 1;
-        gbc.weighty = 1;
-        gbc.insets.set(150, 50, 0, 0);
+
+        var notification = new SSPanel();
+        notification.setBackground(SSWindow.LIGHT_GRAY);
+        var text = new SSLabel("Notifications");
+        text.setPreferredSize(new Dimension(400,100));
+        notification.add(text);
+        gbc.weightx = 0.5;
+        gbc.weighty = 0.5;
+        gbc.insets.set(100, 50, 0, 0);
         gbc.fill = 1;
-        add(notifications, gbc);
+        add(notification, gbc);
+
+        var notifyButton = new SSButton("Show Notifications");
+        notifyButton.setSize(50, 20);
+        notifyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showNotification("ciao");
+            }
+            public static void showNotification(String text){
+                var textField = new SSTextField();
+                text = textField.getText();
+                var visualizza = new SSLabel(text);
+            }
+        });
+        gbc = new GridBagConstraints();
+        gbc.weightx = 0.5;
+        gbc.weighty = 0.5;
+        gbc.gridy = 1;
+        gbc.insets.set(900, 0, 0, 0);
+        gbc.fill = 1;
+        add(notifyButton, gbc);
+    }
+
+    private static void showNotification(String text) {
+        var textField = new SSTextField();
+        text = textField.getText();
+        var visualizza = new SSLabel(text);
+
     }
 }
