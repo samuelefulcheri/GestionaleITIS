7f05bac4f65f0b830e7fc45b2aeb1d6255ccfa6a
samuele.fulcheri <samuele.fulcheri@itiscuneo.eu>
2023-04-22 18:28:48 +0200

    Commit merge

--- src/stocker/storage/view/pages/BorderFactory.java
+++
@@ -1,9 +0,0 @@
-package stocker.storage.view.pages;
-
-import javax.swing.border.Border;
-import java.awt.*;
-
-public class BorderFactory {
-    public static Border createLineBorder(Color black, int i) {
-    }
-}
